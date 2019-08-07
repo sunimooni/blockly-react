149b889d36e07506f61feaa9a96074546818b516
정선 <jspark7373@naver.com>
2019-08-07 15:33:21 +0900

    ad

--- front/src/Three.js
+++ front/src/Three.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import axios from "axios";
 
 class Three extends React.Component{
     state = {
-        isLoading : true
+        isLoading : true,
+        movies : []
     };
+    // 여기서 data를 fetch 한 것이다.
     componentDidMount(){
         setTimeout(() => {
             this.setState({ isLoading : false });
         }, 6000);
+        axios.get("https://yts-proxy.now.sh/list_movies.json");
+
     }
     render() {
         const { isLoading } = this.state;
