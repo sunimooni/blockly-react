793f889eebc2d6bbb387755928cc1609ea807212
정선 <jspark7373@naver.com>
2019-08-07 17:11:08 +0900

    sdfa

--- front/src/Three.js
+++ front/src/Three.js
@@ -9,8 +9,14 @@ class Three extends React.Component{
     // 여기서 data를 fetch 한 것이다.
     getMovies = async() => {
         // async : 이함수가 비동기야, 이거 기다려 => await 한것.
-        const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-        console.log(movies);
+    //    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    //    console.log(movies.data.data.movies);
+        const {
+            data : { 
+                data : {movies}
+            }
+        } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+        this.setState({movies : movies, isLoading : false}); 
     }
     
     componentDidMount(){
