d17a1ef241919d260a8b3dc8fb7c100082e7b6ad
정선 <jspark7373@naver.com>
2019-08-10 02:52:03 +0900

    Sd

--- front/src/Components/Code/Code.js
+++ front/src/Components/Code/Code.js
@@ -10,13 +10,16 @@ class Code extends React.Component{
         return (
 			<section className="code_blank">
 				<BlocklyDrawer
-					tools={[helloWorld]}
+					tools={[helloWorld, test_print]}
 					onChange={(code, workspace) => {console.log(code, workspace);}}
 					appearance={
 						{
 							categories: {
 								Demo: {
-								colour: '270'
+									colour: '270'
+								},
+								test : {
+									colour: '160'
 								},
 							},
 						}
@@ -41,13 +44,11 @@ const helloWorld =  {
         this.jsonInit(
 		{
           message0: 'Hello %1 is %2',
-          args0: [
-            {
+          args0: [{
               type: 'field_input',
               name: 'NAME',
-              check: 'String',
-            },
-          ],
+			  check: 'String', 
+			},],
           output: 'String',
           colour: 160,
           tooltip: 'Says Hello',
@@ -61,5 +62,26 @@ const helloWorld =  {
       return [code, Blockly.JavaScript.ORDER_MEMBER];
     },
   };
+
+const test_print = {
+	name : 'test_print',
+	category : 'test',
+	block : {
+		init : function() {
+			this.jsonInit(
+				{
+						message0 : 'print %1',
+						args0 : [{
+							type : 'field_input',
+							name : 'print',
+						},],
+						colour : 100,
+						tooltip : 'print(_)',
+				}
+			);
+		},
+	},
+};
+
 
 export default Code;
