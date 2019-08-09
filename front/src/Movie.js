926edf7409739d50bbc342e2c2ebe0b1c304efda
sunimooni <jspark7373@naver.com>
2019-08-09 14:16:26 +0900

    sdf

--- front/src/Movie.js
+++ front/src/Movie.js
@@ -3,13 +3,18 @@ import propTypes from "prop-types";
 
 // state 가 필요 없을 경우 function오로
 
-function Movie({id, year, title, summary, poster}){
-    return <div class = "movie">
+function Movie({id, year, title, summary, poster, genres}){
+    return <div className = "movie">
         <img src={poster} alt={title} title={title}></img>
-        <div class="movie_data">
-            <h3 class="movie_title">{title}</h3>
-            <h5 class="movie_year">{year}</h5>
-            <p class="movie_summary">{summary}</p>
+        <div className="movie_data">
+            <h3 className="movie_title">{title}</h3>
+            <h5 className="movie_year">{year}</h5>
+            <p className="movie_summary">{summary}</p>
+	    <ul className="genres">
+		{genres.map((genre, index) => (
+			<li key={index} className="genres_genre">{genre}</li> 
+		))}
+	    </ul>
         </div>
     </div>;
 }
@@ -19,7 +24,8 @@ Movie.propTypes = {
     year : propTypes.number.isRequired,
     title : propTypes.string.isRequired,
     summary : propTypes.string.isRequired,
-    poster : propTypes.string.isRequired
+    poster : propTypes.string.isRequired,
+    genres : propTypes.arrayOf(propTypes.string).isRequired
 };
 
 export default Movie;
--- front/src/Three.js
+++ front/src/Three.js
@@ -28,13 +28,13 @@ class Three extends React.Component{
     }
     render() {
         const { isLoading, movies } = this.state;
-        return <section class = "container">
+        return <section className = "container">
         {isLoading 
-            ? (<div class="loader">
-                <span class="loader_text">Loading...</span>
+            ? (<div className="loader">
+                <span className="loader_text">Loading...</span>
                </div>) 
             : (
-                <div class="movies">
+                <div className="movies">
                     {movies.map(movie => (
                         <Movie 
                             key = {movie.id}
@@ -42,7 +42,8 @@ class Three extends React.Component{
                             year = {movie.year} 
                             title = {movie.title} 
                             summary = {movie.summary} 
-                            poster = {movie.medium_cover_image} 
+                            poster = {movie.medium_cover_image}
+			    genres = {movie.genres} 
                         />))
                     }
                 </div>
