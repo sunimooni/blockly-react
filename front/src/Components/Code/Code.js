a9869aeb8fa2464a6d0e0d0d47432e6872a77d21
정선 <jspark7373@naver.com>
2019-08-10 00:53:55 +0900

    dsf

--- front/src/Components/Code/Code.js
+++ front/src/Components/Code/Code.js
@@ -9,54 +9,84 @@ class Code extends React.Component{
     render(){
         return (
 			<section className="code_blank">
-				<BlocklyDrawer 
-					tools = {[helloWorld]} 
-					onChange = {(code, workspace) => {console.log(code, workspace);}}
-					appearance = {
+				<BlocklyDrawer
+					tools={[helloWorld]}
+					onChange={(code, workspace) => {console.log(code, workspace);}}
+					appearance={
 						{
 							categories: {
-								Demo : {
-									colour : '270'
+								Demo: {
+								colour: '270'
 								},
 							},
 						}
-					}>
+					}
+				>
 					<Category name="Variables" custom="VARIABLE" />
 					<Category name="Values">
 						<Block type="math_number" />
 						<Block type="text" />
-					</Category>	
+					</Category>
 				</BlocklyDrawer>
 			</section>
 		)
     }
 }
 
+  const helloWorld =  {
+    name: 'HelloWorld',
+    category: 'Demo',
+    block: {
+      init: function () {
+        this.jsonInit({
+          message0: 'Hello %1',
+          args0: [
+            {
+              type: 'field_input',
+              name: 'NAME',
+              check: 'String',
+            },
+          ],
+          output: 'String',
+          colour: 160,
+          tooltip: 'Says Hello',
+        });
+      },
+    },
+    generator: (block) => {
+      const message = `'${block.getFieldValue('NAME')}'` || '\'\'';
+      const code = `console.log('Hello ${message}')`;
+      return [code, Blockly.JavaScript.ORDER_MEMBER];
+    },
+  };
+  
+
+
 const helloWorld = {
-	name : 'HelloWorld',
-	Category : 'Demo',
-	block : {
-		init : function(){
-			this.jsonInit({
-				message0 : 'Hello %1',
-				arg0 : [
-					{
-						type : 'field_input',
-						name : 'NAME',
-						check : 'String',
-					},
-				],
-				output : 'String',
-				colour : 160,
-				tooltip : 'Says Hello',
-			});
-		},
-	},
-	generator : (block) => {
-		const message = `'${block.getFieldValue('NAME')}'` || '\'\'';
-    	const code = `console.log('Hello ${message}')`;
-    	return [code, Blockly.JavaScript.ORDER_MEMBER];
-	},
-};
+    name: 'HelloWorld',
+    category: 'Demo',
+    block: {
+      init: function () {
+        this.jsonInit({
+          message0: 'Hello %1',
+          args0: [
+            {
+              type: 'field_input',
+              name: 'NAME',
+              check: 'String',
+            },
+          ],
+          output: 'String',
+          colour: 160,
+          tooltip: 'Says Hello',
+        });
+      },
+    },
+    generator: (block) => {
+      const message = `'${block.getFieldValue('NAME')}'` || '\'\'';
+      const code = `console.log('Hello ${message}')`;
+      return [code, Blockly.JavaScript.ORDER_MEMBER];
+    },
+  };
 
 export default Code;
