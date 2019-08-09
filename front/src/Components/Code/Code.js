6c3eb3ef38d7b0a8abc1f43797ad2157033d5c4b
정선 <jspark7373@naver.com>
2019-08-10 00:48:10 +0900

    Sdf

--- front/src/Components/Code/Code.js
+++ front/src/Components/Code/Code.js
@@ -32,4 +32,31 @@ class Code extends React.Component{
     }
 }
 
+const helloWorld = {
+	name : 'HelloWorld',
+	Category : 'Demo',
+	block : {
+		init : function(){
+			this.jsonInit({
+				message0 : 'Hello %1',
+				arg0 : [
+					{
+						type : 'field_input',
+						name : 'NAME',
+						check : 'String',
+					},
+				],
+				output : 'String',
+				colour : 160,
+				tooltip : 'Says Hello',
+			});
+		},
+	},
+	generator : (block) => {
+		const message = `'${block.getFieldValue('NAME')}'` || '\'\'';
+    	const code = `console.log('Hello ${message}')`;
+    	return [code, Blockly.JavaScript.ORDER_MEMBER];
+	},
+};
+
 export default Code;
