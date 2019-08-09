a1b6a4273a102c89c28e89c8e1c33a382aed3848
정선 <jspark7373@naver.com>
2019-08-09 20:02:39 +0900

    sadf

--- front/src/Components/Header/Header.js
+++ front/src/Components/Header/Header.js
@@ -5,7 +5,15 @@ class Header extends React.Component{
     render(){
         return (
             <div className="Header">
-                
+                <div className="Header_logo">
+                    <h1>DDUK DDAK LEARNING</h1>
+                </div>
+                <div className="User_Info">
+                    <ul>
+                        <li>SignIn</li>
+                        <li>Learn</li>
+                    </ul>
+                </div>
             </div> 
         )
     }
