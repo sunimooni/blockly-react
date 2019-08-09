d30dd1216dc8581021ba07331f15448e12088d17
sunimooni <jspark7373@naver.com>
2019-08-10 00:30:49 +0900

    sdfs

--- front/src/Components/Code/Code.js
+++ front/src/Components/Code/Code.js
@@ -1,11 +1,15 @@
 import React from 'react';
+
+import Blockly from 'node-blockly/browser';
+import BlocklyDrawer, {Block, Category} from 'react-blockly-drawer';
+
 import './Code.css';
 
 class Code extends React.Component{
     render(){
         return (
 	<section className="code_blank">
-		
+		<BlocklyDrawer>	
 	</section>
 	)
     }
