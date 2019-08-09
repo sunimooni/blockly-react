e7e4385f32b37507406eebcbdca656ee74699993
정선 <jspark7373@naver.com>
2019-08-10 00:40:40 +0900

    sdf

--- front/src/Components/Code/Code.js
+++ front/src/Components/Code/Code.js
@@ -8,10 +8,27 @@ import './Code.css';
 class Code extends React.Component{
     render(){
         return (
-	<section className="code_blank">
-		<BlocklyDrawer>	
-	</section>
-	)
+			<section className="code_blank">
+				<BlocklyDrawer 
+					tools = {[helloWorld]} 
+					onChange = {(code, workspace) => {console.log(code, workspace);}}
+					appearance = {
+						{
+							categories: {
+								Demo : {
+									colour : '270'
+								},
+							},
+						}
+					}>
+					<Category name="Variables" custom="VARIABLE" />
+					<Category name="Values">
+						<Block type="math_number" />
+						<Block type="text" />
+					</Category>	
+				</BlocklyDrawer>
+			</section>
+		)
     }
 }
 
