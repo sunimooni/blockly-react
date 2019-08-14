db8cc507cde024fc96d18a75eaf6c78b862a2015
정선 <jspark7373@naver.com>
2019-08-14 23:38:18 +0900

    Sdfs

--- front/src/Components/Code/Code.js
+++ front/src/Components/Code/Code.js
@@ -17,7 +17,7 @@ class Code extends React.Component{
 					<BlocklyDrawer
 						tools={[helloWorld, test_print,test_operation, short_math]}
 						onChange={(code, workspace) => {
-							console.log(code, workspace);
+							console.log(this.props.blocks, this.props.workspace);
 							this.props.storeState(code);
 							this.props.storeWork(workspace);
 						}}
